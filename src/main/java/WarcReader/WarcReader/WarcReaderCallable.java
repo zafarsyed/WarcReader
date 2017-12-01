88317d5eb71bf05878198f40fa75571670b38cb7
CodeMetro <zafar.habeeb1989@gmail.com>
2017-12-01 14:28:25 +0100

    Added sentence spaces to get cleaner text

--- src/main/java/WarcReader/WarcReader/WarcReaderCallable.java
+++ src/main/java/WarcReader/WarcReader/WarcReaderCallable.java
@@ -10,6 +10,7 @@ import java.util.concurrent.Callable;
 import java.util.zip.GZIPInputStream;
 
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
@@ -30,6 +31,7 @@ public class WarcReaderCallable implements Callable<String> {
 	public GZIPInputStream gzInputStream;
 	public DataInputStream inStream;
 	public ObjectMapper mapper;
+	public String articleText;
 	
 	public WarcReaderCallable(String fileLocation, String fileName, String outputLocation) throws FileNotFoundException, IOException
 	{
@@ -50,28 +52,36 @@ public class WarcReaderCallable implements Callable<String> {
 		
 		
 		ObjectNode objectNode1 = mapper.createObjectNode();		
-		while ((thisWarcRecord=WarcRecord.readNextWarcRecord(inStream))!=null) {
+		while ((this.thisWarcRecord=WarcRecord.readNextWarcRecord(inStream))!=null) {
 			// see if it's a response record
-			if (thisWarcRecord.getHeaderRecordType().equals("response")) {
+			if (this.thisWarcRecord.getHeaderRecordType().equals("response")) {
 				docCount++;
 				// it is - create a WarcHTML record
 				//				WarcReader reader = new WarcReaderCompressed();
-				this.htmlRecord=new WarcHTMLResponseRecord(thisWarcRecord);
+				this.htmlRecord=new WarcHTMLResponseRecord(this.thisWarcRecord);
 				// get our TREC ID and target URI
-				String thisTRECID=htmlRecord.getTargetTrecID();
-				String thisTargetURI=htmlRecord.getTargetURI();
-				//System.out.println(Jsoup.parse(htmlRecord.getRawRecord().getContentUTF8()).html());
-				Elements paras = Jsoup.parse(htmlRecord.getRawRecord().getContentUTF8()).select("p");
-				String articleText = "";
+				Document document = Jsoup.parse(this.htmlRecord.getRawRecord().getContentUTF8());
+				String thisTRECID=this.htmlRecord.getTargetTrecID();
+				String thisTargetURI=this.htmlRecord.getTargetURI();
+				String docTitle = document.title();
+				Elements paras = document.select("p");
+				this.articleText = "";
 				for (Element element : paras) {
 					element.select("a").remove();
 					element.select("div").remove();
 					element.select("links").remove();
-					articleText = articleText +" "+ element.text();
-					articleText = articleText.replace("|", "");
-					articleText = articleText.replace("\u00a0", "");
+					String eleText = element.text().replace("\u00a0", "");
+					if(this.articleText.isEmpty() && !(eleText.isEmpty()))
+					{
+						this.articleText = this.articleText + eleText;
+						continue;
+					}
+					if(!(eleText.isEmpty()))
+						this.articleText = this.articleText +" "+ element.text();
 				}
-				if(articleText.isEmpty())
+				this.articleText = this.articleText.replace("|", "");
+				this.articleText = this.articleText.replace("\u00a0", "");
+				if(this.articleText.isEmpty())
 				{
 					//System.out.println("No Text");
 					emptyFiles++;
@@ -80,7 +90,10 @@ public class WarcReaderCallable implements Callable<String> {
 				else
 				{
 					//System.out.println(articleText);
-					objectNode1.put("Article", articleText);
+					if(docTitle.isEmpty())
+						docTitle = "No Document Title";
+					objectNode1.put("Title", docTitle);
+					objectNode1.put("Article", this.articleText);
 					objectNode1.put("URL", thisTargetURI);
 					fw.write("{\"index\": {\"_id\":\""+thisTRECID+"\"}}" + "\n");
 					fw.write(objectNode1.toString() + "\n");
